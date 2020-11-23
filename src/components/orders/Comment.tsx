9562fe26c8339c12419ab87fa98facc89d8c3e49
János Fehér <feherjani96@gmail.com>
2020-11-23 13:37:53 +0100

    Display createdAt on order comments, fix shield icon

--- src/components/orders/Comment.tsx
+++ src/components/orders/Comment.tsx
@@ -1,9 +1,11 @@
-import { Box, Flex, Stack, Text } from "@chakra-ui/react"
+import { Box, Flex, Stack, Text, Tooltip } from "@chakra-ui/react"
+import { format } from "date-fns"
+import hu from "date-fns/locale/hu"
 import { HiShieldCheck } from "react-icons/hi"
 
+import { timeAgo } from "lib/date"
 import { useUser } from "lib/hooks"
 import { CommentWithUser } from "lib/interfaces"
-import HasRole from "components/HasRole"
 import { userrole } from "lib/prismaClient"
 
 interface Props {
@@ -12,16 +14,30 @@ interface Props {
 
 export default function Comment({ data }: Props) {
   const [user] = useUser()
+  const isAdminOrEditor =
+    data.user.role === userrole.ADMIN || data.user.role === userrole.EDITOR
+
   return (
     <>
       {user && (
         <Box rounded="md" p={4} boxShadow="md" mt={4}>
-          <Stack direction="row" spacing={2} as={Flex} alignItems="center" mb={2}>
-            <Text fontSize="sm">{data.user.name}</Text>
-            <HasRole roles={[userrole.ADMIN, userrole.EDITOR]}>
-              <HiShieldCheck />
-            </HasRole>
-          </Stack>
+          <Flex direction="row" justify="space-between">
+            <Stack direction="row" spacing={2} as={Flex} alignItems="center" mb={2}>
+              <Text fontSize="sm">{data.user.name}</Text>
+              {isAdminOrEditor && <HiShieldCheck />}
+            </Stack>
+            <Tooltip
+              placement="top"
+              label={format(new Date(data.createdAt), "yyyy. MMMM dd. HH:mm", {
+                locale: hu,
+              })}
+              aria-label="Küldés ideje"
+            >
+              <Text fontSize="sm" opacity={0.65}>
+                {timeAgo(new Date(data.createdAt))}
+              </Text>
+            </Tooltip>
+          </Flex>
           <Text fontSize="xl">{data.text}</Text>
         </Box>
       )}
